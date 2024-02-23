3ea4fe3ef97f94cc14b07bdfe749201a7b2ff96e
hoor23 <hooriazia23@gmail.com.com>
2024-02-23 18:46:25 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+      productGreen :"#788570"
+      }
+    },
+  },
+  plugins: [],
+}
+
